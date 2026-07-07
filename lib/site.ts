6203145143ec17525bf86d4e74f09d2e676f6f4c
royaviators-creator <royaviators@gmail.com>
2026-07-07 11:29:38 +0200

    feat: centralize Royaviators contact details

---
+++ lib/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "Royaviators",
+  domain: "royaviators.com",
+  contactEmail: "hello@royaviators.com",
+  contactHref: "mailto:hello@royaviators.com",
+  contactCta: "Book a Strategy Session",
+};
